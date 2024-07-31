e17707077da6d04f60864d93b1334e9962132a16
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-07-31 16:35:43 +0300

    functions

--- src/BTS7960_PRO.h
+++ src/BTS7960_PRO.h
@@ -18,10 +18,22 @@
 #define CLOCK_PIN 4
 #define DATA_PIN  2
 
+const int MOTOR_PWM_PIN[QUANTITY_MOTORS] = {6,3,9,5};
+const int MOTOR_DIR_1[QUANTITY_MOTORS] = { // 2,1,0,5
+  0b00000100,
+  0b00000010,
+  0b00000001,
+  0b00100000
+};
+const int MOTOR_DIR_2[QUANTITY_MOTORS] = { // 3,4,7,6
+  0b00001000,
+  0b00010000,
+  0b10000000,
+  0b01000000
+};
+
 class BTS7960_PRO {
   public:
-    int speed[QUANTITY_MOTORS];
-    bool block[QUANTITY_MOTORS];
     void setup() {
       pinMode(LATCH_PIN, OUTPUT);
       pinMode(DATA_PIN, OUTPUT);
@@ -29,28 +41,56 @@ class BTS7960_PRO {
       for (int i = 0; i<QUANTITY_MOTORS; i++) {
         BTS7960_PRO::speed[i] = 0;
         BTS7960_PRO::block[i] = 0;
+        pinMode(MOTOR_PWM_PIN[i],OUTPUT);
       }
     }
-    void run(int number, int speed=0, bool block=0) {
-      if (number<1 || number>QUANTITY_MOTORS) return;
-      BTS7960_PRO::speed[number-1] = speed;
-      BTS7960_PRO::block[number-1] = block;
+    void run(int number, int speed=0, bool update=1) {
+      BTS7960_PRO::runBasic(number,speed,0,update);
+    }
+    void runs(int a=0, int b=0, int c=0, int d=0) {
+      BTS7960_PRO::run(1,a,0);
+      BTS7960_PRO::run(2,b,0);
+      BTS7960_PRO::run(3,c,0);
+      BTS7960_PRO::run(4,d);
+    }
+    void blockMotor(int number, bool update=1) {
+      BTS7960_PRO::runBasic(number,0,1,update);
+    }
+    void blockMotors() {
+      for (int i = 0; i<QUANTITY_MOTORS; i++) {
+        BTS7960_PRO::blockMotor(i,0);
+      }
       BTS7960_PRO::update();
     }
-    void update() { // full
-      // bitSet(direction, currentLED);
+    void runWithBlock(int number, int speed=0, bool update=1) {
+      if (speed==0) blockMotor(number,update);
+      else run(number,speed,update);
+    }
+    void runWithBlocks(int a=0, int b=0, int c=0, int d=0) {
+      BTS7960_PRO::runWithBlock(1,a,0);
+      BTS7960_PRO::runWithBlock(2,b,0);
+      BTS7960_PRO::runWithBlock(3,c,0);
+      BTS7960_PRO::runWithBlock(4,d);
+    }
+  private:
+    int speed[QUANTITY_MOTORS];
+    bool block[QUANTITY_MOTORS];
+    void runBasic(int number, int speed=0, bool block=0, bool update=1) {
+      if (number<1 || number>QUANTITY_MOTORS) return;
+      BTS7960_PRO::speed[number-1] = constrain(speed,-100,100)*2.5*int(!block);
+      BTS7960_PRO::block[number-1] = block;
+      if (update) BTS7960_PRO::update();
+    }
+    void update() {
       int direction = 0;
       for (int i = 0; i<QUANTITY_MOTORS; i++) {
-        int a = 0;
-        if (BTS7960_PRO::block[i]) a = 3;
-        else if (BTS7960_PRO::speed[i]>0) a = 1;
-        else if (BTS7960_PRO::speed[i]<0) a = 2;
-        direction = direction<<2 + a;
+        if (BTS7960_PRO::block[i]) direction += MOTOR_DIR_1[i] + MOTOR_DIR_2[i];
+        else if (BTS7960_PRO::speed[i]>0) direction += MOTOR_DIR_1[i];
+        else if (BTS7960_PRO::speed[i]<0) direction += MOTOR_DIR_2[i];
+        analogWrite(MOTOR_PWM_PIN[i],abs(BTS7960_PRO::speed[i]));
       }
       digitalWrite(LATCH_PIN, LOW);
       shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, direction);
       digitalWrite(LATCH_PIN, HIGH);
-      
     }
-  private:
 };
